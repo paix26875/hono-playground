7adaf830c8502625057edc6634d63a2b714ccf54
paix26875 <66575727+paix26875@users.noreply.github.com>
2025-08-26 09:44:21 +0900

    chore: Helloworld

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,12 +1,20 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.get('/welcome', (c) => {
+  return c.text('Welcome to Hono!')
+});
+app.get('/*', basicAuth({
+  username: 'hono',
+  password: 'acoolproject',
+}))
+app.get('/hello', (c) => {
   return c.text('Hello Hono!')
 })
-
 serve({
   fetch: app.fetch,
   port: 3000
