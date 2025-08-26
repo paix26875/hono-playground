46f0dd11d506baccba1b0bd775569871b5514870
paix26875 <66575727+paix26875@users.noreply.github.com>
2025-08-26 12:10:36 +0900

    NODE_ENVの設定を入れた

--- cloudflare-workers-vite/src/index.tsx
+++ cloudflare-workers-vite/src/index.tsx
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
+import {logger} from "hono/logger";
 
 const app = new Hono()
 
 app.use(renderer)
+app.use(logger())
 
 app.get('/', (c) => {
   return c.render(<h1>Hello World! MyFirst Cloudflare Workers</h1>)
